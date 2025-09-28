d16099889fca65c673d20fd18cafe2dd7c2b4285
QuiPau <pquidu@ip-172-31-20-151.ec2.internal>
2025-09-28 00:58:32 +0000

    Script to extract from API

--- assignment5/script/test.py
+++ assignment5/script/test.py
@@ -2,12 +2,36 @@ import requests
 import pandas as pd
 
 url = "https://data.cityofnewyork.us/resource/jrb2-thup.json"
-response = requests.get(url)
 
-if response.status_code == 200:
-    print("Success!")
+limit = 1000
+offset = 0
+first_chunk = True
+
+while True:
+    response = requests.get(url, params={"$limit": limit, "$offset": offset}, timeout=10)
+    response.raise_for_status()  # will raise an error if request fails
+
     data = response.json()
-    df = pd.DataFrame(data)  # create DataFrame directly from JSON
-    df.to_csv('~/repo/comp370-2025/assignment5/data/NYU_dataset.csv', encoding='utf-8', index=False)
-else:
-    print(f"Request failed with status code {response.status_code}")
+    if not data:
+        break
+
+    df = pd.DataFrame(data)
+    df.to_csv(
+        "/home/pquidu/repo/comp370-2025/assignment5/data/NYU_data.csv",
+        mode='w' if first_chunk else 'a',
+        header=first_chunk,
+        index=False,
+        encoding='utf-8'
+    )
+
+    first_chunk = False
+    offset += limit~
+    print(f"Fetched {offset} rows...")
+
+data=[]
+
+""" response = requests.get(url)                #https://stackoverflow.com/questions/60191014/valueerror-invalid-file-path-or-buffer-object-type-class-dict-python
+content = json.loads(response.text)
+data = pd.DataFrame(content)
+print (type(data))
+print(len(data)) """
